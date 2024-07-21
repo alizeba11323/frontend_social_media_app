b12e01f487a84028306d0e6a95e73905e9621626
alizeba11323 <alizeba11323@gmail.com>
2024-07-21 12:58:27 +0530

    Cookie Checked

--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../store/auth.store";
 
 function ProtectedLayout() {
+  console.log(document.cookie);
   const { authenticated } = useAuth((state) => ({
     authenticated: state.authenticated,
   }));
